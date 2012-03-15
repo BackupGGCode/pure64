e4176f0ff609424a270e1fb329916e4367668f05
iseyler@gmail.com <iseyler@gmail.com@588fd83d-545d-496b-a9fc-ab0b867fb1ba>
2012-03-15 16:53:37 +0000

    Initial work on HPET support.

---
+++ trunk/init_hpet.asm
@@ -0,0 +1,13 @@
+; =============================================================================
+; Pure64 -- a 64-bit OS loader written in Assembly for x86-64 systems
+; Copyright (C) 2008-2012 Return Infinity -- see LICENSE.TXT
+;
+; INIT HPET
+; =============================================================================
+
+
+init_hpet:
+; the first HPET chip should be at 0xFED00000
+
+; =============================================================================
+; EOF
